11f5730157f0b8de0ca5813656ddb45d5b964207
joeyoungmin <quswmf45@naver.com>
2024-12-11 17:11:52 +0900

    ✨ : time-gps & navigation 기능 생성
    - time-gps 컴포넌트 생성
    -navigation 컴포넌트 및 기능 생성

---
+++ src/components/navigation/navigation.ts
@@ -0,0 +1,75 @@
+import { html, css, LitElement } from 'lit';
+import "./navigation.scss";
+import "../../assets/sass/reset.scss"
+import "../../assets/sass/variables.scss"
+import "../../assets/sass/font.scss";
+
+class Navigation extends LitElement {
+  static styles = css`
+
+
+    ul, li {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      font-family: 'Paperlogy', sans-serif;
+      font-weight: 300;
+    }
+
+    ul {
+      height: 32px;
+      display: flex;
+      justify-content: space-around;
+      background-color: var(--foundation-secondary-color);
+      padding: 10px 10px;
+      box-sizing: border-box;
+      overflow: hidden;
+      line-height: 0.8;
+    }
+
+    ul li {
+      color: var(--text-light);
+      transition: 0.2s;
+      cursor: pointer;
+    }
+
+    ul li:hover {
+      color: var(--foundation-primary-color);
+      border-bottom: 3px solid var(--foundation-primary-color);
+      height: 20px;
+    }
+  `;
+
+  handleNavigation(event: MouseEvent) {
+    const target = event.target as HTMLLIElement;
+    switch (target.textContent) {
+      case '피드':
+        window.location.href = '/feed';
+        break;
+      case '방문':
+        window.location.href = '/visit';
+        break;
+      case '리뷰':
+        window.location.href = '/review';
+        break;
+      case '예약•주문':
+        window.location.href = '/reservation';
+        break;
+      default:
+        break;
+    }
+  }
+
+  render() {
+    return html`
+      <ul class="navigation-wrap">
+        <li @click=${this.handleNavigation}>피드</li>
+        <li @click=${this.handleNavigation}>방문</li>
+        <li @click=${this.handleNavigation}>리뷰</li>
+        <li @click=${this.handleNavigation}>예약•주문</li>
+      </ul>
+    `;
+  }
+}
+
+customElements.define('navi-gation', Navigation);
