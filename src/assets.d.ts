61348fdb70eafcd78ec70dcb0630116349dc8d7a
BomEllen <jjjj200100@gmail.com>
2024-12-13 15:18:03 +0900

    ✨ : navitems 컴포넌트 생성
    - navitems(네브바) 컴포넌트 생성
    
    resolves #10

---
+++ src/assets.d.ts
@@ -0,0 +1,19 @@
+declare module "*.svg" {
+    const content: string;  // 파일 경로를 string으로 처리
+    export default content;
+  }
+
+  declare module "*.png" {
+    const content: string;  // 파일 경로를 string으로 처리
+    export default content;
+  }
+
+  declare module "*.jpg" {
+    const content: string;  // 파일 경로를 string으로 처리
+    export default content;
+  }
+
+  declare module "*.scss" {
+    const content: string;  // SCSS 모듈을 string으로 처리
+    export default content;
+  }
--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,19 @@ import { defineConfig } from 'vite'
 
 
 export default defineConfig({
+  base: "./",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"), // '@'를 'src' 디렉토리로 매핑
+    },
+  },
   build: {
-    outDir:'docs',
+    outDir: "docs",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        
+        main: resolve(__dirname, "index.html"),
+        navitems: resolve(__dirname, "src/layout/navitems/index.html"),
       },
     },
-    
   },
-})
+});
