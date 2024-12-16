3e02eb0fcca809909b77114ffa7f8824f3e4c421
kongsh <ksh080838@gmail.com>
2024-12-16 14:41:30 +0900

    🔧: 포켓호스트 연결 및 config 파일 변경
    - 포켓호스트 서버 주소로 연결
    - vite.config.js 파일에 중복되는 옵션 제거
    
    Resolved #8

--- src/api/pocketbase.ts
+++ src/api/pocketbase.ts
@@ -1,4 +1,4 @@
 import PocketBase from "pocketbase";
 
-const pb: PocketBase = new PocketBase("http://127.0.0.1:8090");
+const pb: PocketBase = new PocketBase("https://compass-mighty.pockethost.io/");
 export default pb;
