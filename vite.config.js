9e7821d1b87637cd8a06a140fe25a8f3ba2beed2
leeginger <onepagecode@naver.com>
2024-12-16 11:10:59 +0900

    🔧: 환경 구성
    - 환경 구성 완료

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import inlineCssModules from "vite-plugin-inline-css-modules";
 
 export default defineConfig({
+  plugins: [inlineCssModules()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "/src"), // '@'를 'src' 디렉토리로 매핑
+    },
+  },
   build: {
-    outDir:'docs',
+    outDir: "docs",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        
+        main: resolve(__dirname, "index.html"),
+        profileAll: resolve(__dirname, "src/components/profile-all/index.html"),
       },
     },
-    
   },
-})
+});
