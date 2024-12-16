4b6a40f337089069931c98b8ed109a802db3049b
kongsh <ksh080838@gmail.com>
2024-12-16 15:50:17 +0900

    ♻️ : login, register 코드 재구성
    - login, register.ts에 공통되는 함수들을 sign-form.ts 파일에 정리했습니다.

---
+++ src/utils/sign-form.ts
@@ -0,0 +1,52 @@
+const idRegex = /^[A-Za-z0-9]{3,}$/;
+const emainRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const pwRegex = /^(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/;
+
+export function isSignUpValid(inputList: HTMLInputElement[]): boolean {
+  let count = 0;
+
+  for (let item of inputList) {
+    switch (item.id) {
+      case "id-field":
+        if (idRegex.test(item.value)) {
+          count++;
+        }
+        break;
+      case "email-field":
+        if (emainRegex.test(item.value)) {
+          count++;
+        }
+        break;
+      case "pw-field":
+        if (pwRegex.test(item.value)) {
+          count++;
+        }
+        break;
+      case "check-pw-field":
+        if (inputList[2].value === item.value) {
+          count++;
+        }
+        break;
+    }
+  }
+
+  if (count === inputList.length) {
+    return true;
+  }
+  return false;
+}
+
+export function handleInput(inputList: HTMLInputElement[], loginBtn: HTMLButtonElement) {
+  if (isSignUpValid(inputList)) {
+    loginBtn.disabled = false;
+  } else {
+    loginBtn.disabled = true;
+  }
+}
+
+export function alertAndProceed(msg: string): Promise<void> {
+  return new Promise((res) => {
+    alert(msg);
+    res();
+  });
+}
