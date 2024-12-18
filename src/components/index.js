10169c28e4dfc3336b5253da7709f255f8b4be7c
kongsh <ksh080838@gmail.com>
2024-12-18 20:53:42 +0900

    🎨 : index.js로 모듈 export
    - index.js로 components, layout의 모듈들을 묶어서 export

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import "./location/location.ts";
+import "./navigation/navigation.ts";
+import "./post/post.ts";
+import "./post-list/post-list.ts";
+import "./profile-all/profile-all.ts";
+import "./theme/theme.ts";
