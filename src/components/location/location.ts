2351039f34a84d9bc84aa620b9dce11425624257
kongsh <ksh080838@gmail.com>
2024-12-19 13:54:21 +0900

    🎨 : svg 파일 shadowDOM 내부에 그대로 작성
    - svg 파일 shadowDOM 내부에 그대로 작성

--- src/components/location/location.ts
+++ src/components/location/location.ts
@@ -1,7 +1,6 @@
 import { html, LitElement, css, CSSResultGroup, unsafeCSS } from "lit";
 import { customElement } from "lit/decorators.js";
 import styles from "./location.scss?inline";
-import map from "@/assets/navitems/map.svg";
 
 @customElement("location-bar")
 class Location extends LitElement {
@@ -89,7 +88,14 @@ class Location extends LitElement {
   render() {
     return html`
       <nav class="location-nav">
-        <button class="btn-location" type="button"><img src="${map}" alt="현 위치 로고" /><span>현위치</span></button>
+        <button class="btn-location" type="button">
+          <svg role="img" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512" fill="#19172E">
+            <title>현 위치 로고</title>
+            <path d="M12,6a4,4,0,1,0,4,4A4,4,0,0,0,12,6Zm0,6a2,2,0,1,1,2-2A2,2,0,0,1,12,12Z" />
+            <path d="M12,24a5.271,5.271,0,0,1-4.311-2.2c-3.811-5.257-5.744-9.209-5.744-11.747a10.055,10.055,0,0,1,20.11,0c0,2.538-1.933,6.49-5.744,11.747A5.271,5.271,0,0,1,12,24ZM12,2.181a7.883,7.883,0,0,0-7.874,7.874c0,2.01,1.893,5.727,5.329,10.466a3.145,3.145,0,0,0,5.09,0c3.436-4.739,5.329-8.456,5.329-10.466A7.883,7.883,0,0,0,12,2.181Z" />
+          </svg>
+          <span>현위치</span>
+        </button>
 
         <ul class="location-menu" @focusin=${this.handleFocus} @mousedown=${this.handleMouseDown} @mousemove=${this.handleMouseMove} @mouseup=${this.handleMouseUp} @mouseleave=${this.handleMouseLeave}>
           <li><button class="active" type="button">전체</button></li>
