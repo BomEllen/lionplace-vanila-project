9f5399bd9cfa8f4f2a3f6a101ea27d86042ce755
leeginger <onepagecode@naver.com>
2024-12-19 15:28:21 +0900

    🔧: visit-record 페이지 추가
    - visit-record 페이지에 컴포넌트 추가
    
    Resolved #32

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ import "./post/post.ts";
 import "./post-list/post-list.ts";
 import "./profile-all/profile-all.ts";
 import "./theme-bar/theme-bar.ts";
+import "./visit-record/visit-record-complete.ts";
+import "./visit-record/visit-record-noreview.ts";
