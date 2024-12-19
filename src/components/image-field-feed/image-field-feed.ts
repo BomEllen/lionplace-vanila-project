8b644854d7ed8624f6f85e3282c1c28d4eae9b3a
joeyoungmin <quswmf45@naver.com>
2024-12-19 16:32:37 +0900

    🎨:review-component 접근성 문제 해결
    - role="img" 추가
    - title추가가

--- src/components/image-field-feed/image-field-feed.ts
+++ src/components/image-field-feed/image-field-feed.ts
@@ -111,13 +111,15 @@ class ImageFieldFeed extends LitElement {
               (item) => html`
                 <section class="image-text-container">
                   <img src="${item.img}" alt="${item.title}" />
-                  <svg 
+                  <svg
+                    role="img"
                     id="bookmark" 
                     @click="${() => this.toggleBookmark(item.id)}"
                     xmlns="http://www.w3.org/2000/svg" 
                     viewBox="0 0 24 24" 
                     width="24" height="24"
                     fill="${item.isBookmarked ? '#FF7710' : '#aaa'}">
+                    <title>북마크</title>
                     <path d="M2.3999 2.4C2.3999 1.08 3.4799 0 4.7999 0H19.1999C19.8364 0 20.4469 0.252856 20.897 0.702944C21.347 1.15303 21.5999 1.76348 21.5999 2.4V24L11.9999 19.2L2.3999 24V2.4Z" />
                   </svg>
                   <div class="text-overlay">
