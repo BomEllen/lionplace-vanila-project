af1ca453470ef65ccbe500d7d23d25189460edbb
kongsh <ksh080838@gmail.com>
2024-12-19 17:38:52 +0900

    ♻️ : 네비게이션 경로 수정
    - a태그에 경로 수정

--- src/components/navigation/navigation.ts
+++ src/components/navigation/navigation.ts
@@ -15,13 +15,13 @@ class Navigation extends LitElement {
         window.location.href = "/src/pages/feed/";
         break;
       case "방문":
-        window.location.href = "/visit";
+        window.location.href = "/src/pages/visit-record/";
         break;
       case "리뷰":
-        window.location.href = "/review";
+        window.location.href = "/src/pages/review/";
         break;
       case "예약•주문":
-        window.location.href = "/reservation";
+        window.location.href = "/src/pages/reserved/";
         break;
       default:
         break;
@@ -33,16 +33,16 @@ class Navigation extends LitElement {
       <nav role="navigation" aria-label="주요 네비게이션">
         <ul class="navigation-wrap">
           <li>
-            <a href="/feed" @click=${this.handleNavigation} aria-label="피드로 이동" tabindex="0"> 피드 </a>
+            <a href="/src/pages/feed/" @click=${this.handleNavigation} aria-label="피드로 이동" tabindex="0"> 피드 </a>
           </li>
           <li>
-            <a href="/visit" @click=${this.handleNavigation} aria-label="방문 페이지로 이동" tabindex="0"> 방문 </a>
+            <a href="/src/pages/visit-record/" @click=${this.handleNavigation} aria-label="방문 페이지로 이동" tabindex="0"> 방문 </a>
           </li>
           <li>
-            <a href="/review" @click=${this.handleNavigation} aria-label="리뷰 페이지로 이동" tabindex="0"> 리뷰 </a>
+            <a href="/src/pages/review/" @click=${this.handleNavigation} aria-label="리뷰 페이지로 이동" tabindex="0"> 리뷰 </a>
           </li>
           <li>
-            <a href="/reservation" @click=${this.handleNavigation} aria-label="예약 및 주문 페이지로 이동" tabindex="0"> 예약•주문 </a>
+            <a href="/src/pages/reserved/" @click=${this.handleNavigation} aria-label="예약 및 주문 페이지로 이동" tabindex="0"> 예약•주문 </a>
           </li>
         </ul>
       </nav>
