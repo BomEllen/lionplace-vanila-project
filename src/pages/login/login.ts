27897df36877ed85a671f0efc3601ccdb732dfd5
kongsh <ksh080838@gmail.com>
2024-12-20 16:53:10 +0900

    🚧 : login.ts 로그인 시 데이터 저장
    - login.ts 로그인 시 데이터 저장

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -14,15 +14,21 @@ async function handleLogin(e: Event) {
   const [userName, pw] = [inputList[0].value, inputList[1].value];
 
   try {
-    const authData = await pb.collection("users").authWithPassword(userName, pw);
+    const { record, token } = await pb.collection("users").authWithPassword(userName, pw);
 
-    console.log(authData);
+    localStorage.setItem(
+      "auth",
+      JSON.stringify({
+        record,
+        token,
+      })
+    );
 
     console.log("로그인 성공");
 
-    alertAndProceed("로그인 성공! 확인 버튼을 누르시면 메인 페이지로 이동합니다.").then(() => {
-      location.href = "/src/pages/feed/"; // 피드 페이지로 이동
-    });
+    // alertAndProceed("로그인 성공! 확인 버튼을 누르시면 메인 페이지로 이동합니다.").then(() => {
+    //   location.href = "/src/pages/feed/"; // 피드 페이지로 이동
+    // });
   } catch (err) {
     // 차후 로그인 실패 시 알림 처리 예정
     console.log("로그인 실패");
