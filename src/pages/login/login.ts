e031dda62dac29767a8b6439d7a5062dbb31402c
kongsh <ksh080838@gmail.com>
2024-12-24 21:05:18 +0900

    💄 : 코드 수정
    - profile-all : 리뷰 작성 버튼 삭제, localStorage의 데이터로 사진, 닉네임 적용
    - time-gps : auth 데이터 있는지 확인 후 없다면(비로그인) 리디렉션
    - login 페이지 : 버튼 활성화 방식 변경, label 추가
    - register 페이지 : 버튼 활성화 방식 변경, 틀렸을 시 애니메이션 추가, 첫 사용자 기본 이미지 추가
    - mixin : shake-animation 추가
    - form-utils : debounce 함수 추가
    - variables : 색상 접근성 체크 후 새 주황색 적용

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,7 +2,7 @@ import "../../styles/sass/reset.scss";
 import "../../styles/sass/font.scss";
 import "./login.scss";
 import pb from "../../api/pocketbase";
-import { alertAndProceed, handleInput } from "../../utils/form-utils";
+import { handleInput, debounce, alertAndProceed } from "../../utils/form-utils";
 
 const inputList = [...document.querySelectorAll("input")];
 const loginBtn = document.querySelector(".btn-login") as HTMLButtonElement;
@@ -26,9 +26,9 @@ async function handleLogin(e: Event) {
 
     console.log("로그인 성공");
 
-    // alertAndProceed("로그인 성공! 확인 버튼을 누르시면 메인 페이지로 이동합니다.").then(() => {
-    //   location.href = "/src/pages/feed/"; // 피드 페이지로 이동
-    // });
+    alertAndProceed("로그인 성공! 확인 버튼을 누르시면 메인 페이지로 이동합니다.").then(() => {
+      location.href = "/src/pages/feed/"; // 피드 페이지로 이동
+    });
   } catch (err) {
     // 차후 로그인 실패 시 알림 처리 예정
     console.log("로그인 실패");
@@ -37,8 +37,9 @@ async function handleLogin(e: Event) {
 }
 
 inputList.forEach((item) => {
-  item.addEventListener("change", () => {
-    handleInput(inputList, loginBtn);
+  item.addEventListener("input", () => {
+    const debounceHandleInput = debounce(handleInput, 300);
+    debounceHandleInput(inputList, loginBtn);
   });
 });
 
